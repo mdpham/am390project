b5fe2040453978616807dc039f8e33112855a0fb
Martin Pham <mdpham@uwaterloo.ca>
2016-11-24 00:08:33 -0500

    comp and smooth and play

---
+++ comp_script.m
@@ -0,0 +1,20 @@
+wname = 'db2';
+N = [5 4 3 2 1];
+
+err = [];
+energy = [];
+retained = [];
+reduced = [];
+p = 0:5:100;
+for i = 1:length(p)
+    P = ones(1,5)*p(i);
+    [xcomp,t, er, ener, ret, red] = wavelet_comp(N,P,wname);
+    err = [err er];
+    energy = [energy ener];
+    retained = [retained ret];
+    reduced = [reduced red];
+end
+
+wname
+
+[p' 100*reduced' 100*err' energy']
