f84899225c10f8b51e915b28530163c0419562b7
Martin Pham <mdpham@uwaterloo.ca>
2016-11-23 20:22:37 -0500

    wavelet comp for violin

--- wavelet_comp.m
+++ wavelet_comp.m
@@ -1,6 +1,13 @@
-function [f] = wavelet_comp(X,t, N, P, wname)
+function [f] = wavelet_comp(N, P, wname)
+
+[X,Fs] = audioread('/audio/violin.wav');
+X = mean(X')';
+dt = 1/Fs;
+t = 0:dt:(length(X)*dt - dt);
 
 [coeff, l] = wavedec(X, 5, wname);
+
+
 
 nc = wthcoef('d',coeff,l,N,P);
 
@@ -8,9 +15,13 @@ xcomp = waverec(nc,l,wname);
 
 figure(1)
 hold on
-plot(t,X,'k-')
-plot(t,xcomp,'b--')
+plot(t,X,'k-');
+plot(t,xcomp,'b--');
 
 f = xcomp;
-norm(X-xcomp)
+norm(X-xcomp)   
+sum(coeff == 0)
+sum(nc == 0)
+
+(sum(nc == 0) - sum(coeff == 0)) / length(coeff)
 end
